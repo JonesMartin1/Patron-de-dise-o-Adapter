74af40420125d7e7eeff687ddbdd07a04376f003
Martin <martin.jones.r@gmail.com>
2022-05-24 18:53:43 -0300

    Elimine un test que está de mas

--- Emailer/Adaptee.ts
+++ Emailer/Adaptee.ts
@@ -1,8 +1,8 @@
 //Para implementar un nuevo proveedor de correo utilizaremos el patron adapter para que esto se mas facil
 
-//NUevo proveedor de mails que dispone de una interfaz diferente llamda mandarEmail (Adaptee)
+//Nuevo proveedor de mails que dispone de una interfaz diferente mandarEmail (Adaptee)
 export class EmailProvider2{
-    public mandarEmail(): void{
-        console.log("Enviar mail por nuevo proveedor");
+    public mandarEmail(): string{
+       return("Enviar mail por nuevo proveedor");
     }
 }
